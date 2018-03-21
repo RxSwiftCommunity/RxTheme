8b7a926bbc42b5e64df74a90d2452f314c37ce15
duan <wddwyss@gmail.com>
2018-03-21 10:31:06 +0900

    Remove redundant binder

--- RxTheme.podspec
+++ RxTheme.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'RxTheme'
-  s.version          = '1.0.0'
+  s.version          = '1.0.1'
   s.swift_version    = '4.0'
   s.summary          = 'Theme management based on RxCocoa'
   s.description      = <<-DESC
--- RxTheme/Classes/Extensions/UIButton+Rx.swift
+++ RxTheme/Classes/Extensions/UIButton+Rx.swift
@@ -11,25 +11,15 @@ import RxCocoa
 
 
 public extension Reactive where Base: UIButton {
-    /// Bindable sink for `setImage(<>, for: [])`
-    public var image: Binder<UIImage?> {
-        return Binder(self.base) { view, attr in
-            view.setImage(attr, for: [])
-        }
-    }
-
-    /// Bindable sink for `setBackgroundImage(<>, for: [])`
-    public var backgroundImage: Binder<UIImage?> {
-        return Binder(self.base) { view, attr in
-            view.setBackgroundImage(attr, for: [])
-        }
-    }
 
     /// Bindable sink for `tintColor` property
     public var tintColor: Binder<UIColor?> {
         return Binder(self.base) { view, attr in
-            view.tintColor = attr
+            UIView.animate(withDuration: 0.3, animations: {
+                view.tintColor = attr
+            })
         }
     }
+
 }
 #endif
