d7026e660a406795e3cbd0ac489556007c55af8d
duan <wddwyss@gmail.com>
2018-09-22 15:28:22 +0900

    Upgrade RxSwift to fix cocoapods

--- RxTheme/Classes/Theme.swift
+++ RxTheme/Classes/Theme.swift
@@ -6,7 +6,7 @@
 import RxSwift
 import RxCocoa
 
-public protocol ThemeProvider {
+public protocol ThemeProvider: Hashable {
     associatedtype T
     var associatedObject: T { get }
 }
@@ -22,36 +22,45 @@ public class ThemeService<Provider: ThemeProvider>: NSObject {
     fileprivate init(initial: Provider) {
         self.relay = BehaviorRelay<Provider>(value: initial)
     }
+    private var cache = [Provider: Provider.T]()
+    private func getAssociateedObject(_ key: Provider) -> Provider.T {
+        if let cached = cache[key] { return cached }
+        let object = key.associatedObject
+        cache[key] = object
+        return object
+    }
     /// BehaviorRelay for theme
     public let relay: BehaviorRelay<Provider>
     /// Current theme
     public var theme: Provider { return self.relay.value }
     /// Current theme object
-    public var themeObject: Provider.T { return self.theme.associatedObject }
+    public var themeObject: Provider.T { return self.getAssociateedObject(self.theme) }
     /// Set theme
     public func set(_ theme: Provider) {
         self.relay.accept(theme)
     }
     /// Start binding
-    public var rx: ThemeBindable<Provider> {
-        return ThemeBindable<Provider>(relay: self.relay)
+    public var rx: ThemeBindable<Provider.T> {
+        let targetRelay = self.relay.map {
+            self.getAssociateedObject($0)
+        }
+        return ThemeBindable(relay: targetRelay)
     }
 }
 
-public class ThemeBindable<Provider: ThemeProvider> {
-    private let relay: BehaviorRelay<Provider>
+public class ThemeBindable<T> {
+    private let relay: Observable<T>
     private var disposables: [Disposable]
 
-    fileprivate init(relay: BehaviorRelay<Provider>) {
+    fileprivate init(relay: Observable<T>) {
         self.relay = relay
         self.disposables = []
     }
 
     /// Bind theme component to UI attributes
-    public func bind<U>(_ from: @escaping ((Provider.T) -> U), to binders: [Binder<U>]) -> ThemeBindable {
+    public func bind<U>(_ from: @escaping ((T) -> U), to binders: [Binder<U>]) -> ThemeBindable {
         disposables += binders.map {
-            // todo: put $0.associatedObject to ThemeService
-            self.relay.map { $0.associatedObject }.map(from)
+            self.relay.map(from)
                 .observeOn(MainScheduler.instance)
                 .bind(to: $0)
         }
@@ -59,7 +68,7 @@ public class ThemeBindable<Provider: ThemeProvider> {
     }
 
     /// Bind theme component to UI attributes
-    public func bind<U>(_ from: @escaping ((Provider.T) -> U), to binders: Binder<U>...) -> ThemeBindable {
+    public func bind<U>(_ from: @escaping ((T) -> U), to binders: Binder<U>...) -> ThemeBindable {
         return self.bind(from, to: binders)
     }
 
