4031ad578a5567144d875022bd426ac0910d44d3
duan <wddwyss@gmail.com>
2019-01-24 15:11:06 +0900

    Remove UIButton animation

--- RxTheme/Classes/RxExtensions/UIButton+Rx.swift
+++ RxTheme/Classes/RxExtensions/UIButton+Rx.swift
@@ -15,9 +15,7 @@ public extension Reactive where Base: UIButton {
     /// Bindable sink for `titleColor` property
     public func titleColor(for state: UIControl.State) -> Binder<UIColor?> {
         return Binder(self.base) { view, attr in
-            UIView.animate(withDuration: 0.3, animations: {
-                view.setTitleColor(attr, for: state)
-            })
+            view.setTitleColor(attr, for: state)
         }
     }
     
